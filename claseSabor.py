74fc21869b573f40591798e9a06fc08f766c4981
cg19 <132721288+cgrp19@users.noreply.github.com>
2023-06-07 16:24:25 -0300

    Add files via upload

---
+++ claseSabor.py
@@ -0,0 +1,9 @@
+class Sabor:
+    __idSabor = 'int'
+    __nombreSabor = 'string'
+    __ingredientes = 'string'
+
+    def __init__(self, idSabor, nombreSabor, ingredientes):
+        self.__idSabor = idSabor
+        self.__nombreSabor = nombreSabor
+        self.__ingredientes = ingredientes
